428f962212841e26028db5411c43165b3b984a02
raniaahmed113 <79448002+raniaahmed113@users.noreply.github.com>
2021-07-07 16:23:54 +0200

    Add files via upload

--- www/toasterPlugin.js
+++ www/toasterPlugin.js
@@ -9,8 +9,8 @@ ToasterPlugin.prototype.show = function(message, duration, successCallback, erro
   options.duration = duration;
   cordova.exec(successCallback, errorCallback, 'ToasterPlugin', 'show', [options]);
 }
-ToasterPlugin.prototype.new_activity = function() {
-    exec(function(res){}, function(err){}, "ToasterPlugin", "new_activity", []);
+ToasterPlugin.prototype.new_activity = function( successCallback, errorCallback) {
+    exec( successCallback, errorCallback, "ToasterPlugin", "new_activity", []);
 
 // Installation constructor that binds ToasterPlugin to window
 ToasterPlugin.install = function() {
