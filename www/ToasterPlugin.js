21656fdb1fc352842ef4df8a94f41516a1724811
raniaahmed113 <79448002+raniaahmed113@users.noreply.github.com>
2021-07-08 15:31:36 +0200

    Add files via upload

--- www/ToasterPlugin.js
+++ www/ToasterPlugin.js
@@ -1,8 +1,15 @@
-var exec = require('cordova/exec');
+function ToasterPlugin() {}
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'ToasterPlugin', 'coolMethod', [arg0]);
-};
 ToasterPlugin.prototype.new_activity = function( successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'ToasterPlugin', 'new_activity', []);
 }
+
+ToasterPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.toasterPlugin = new ToasterPlugin();
+  return window.plugins.toasterPlugin;
+};
+
+cordova.addConstructor(ToasterPlugin.install);
